202c1d2597dcad7fbf62a158c694f725925f457a
Marcin <marcinjaj@gmail.com>
2016-06-02 18:53:29 +0200

    drobny update GUI

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -3,8 +3,16 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Dylek on 2016-05-27.
@@ -14,6 +22,8 @@ public class MainWindow extends JFrame {
 
     private JTextArea generatedOutput ;
     private JTextArea loadedLogicRules;
+    private ArrayList <String> loadedFileLines;
+
     private JFileChooser fileChooser;
     private JMenuBar menuBar;
     private JButton generateB;
@@ -24,18 +34,18 @@ public class MainWindow extends JFrame {
 
     public MainWindow(){
         mainFrame=new JFrame();
-        JTextArea generatedOutput = new JTextArea();
-        JTextArea loadedLogicRules = new JTextArea();
+        generatedOutput = new JTextArea("");
+        loadedLogicRules = new JTextArea("");
         generateB=new JButton("Generate");
         loadFileB=new JButton("Open File");
         saveFileB=new JButton("Save File");
         fileChooser=new JFileChooser();
-
+        loadedFileLines=new ArrayList<String>();
         rulesPanel=new JScrollPane(loadedLogicRules);
         outputPanel=new JScrollPane(generatedOutput);
 
-        rulesPanel.setPreferredSize(new Dimension(500,300));
-        outputPanel.setPreferredSize(new Dimension(500,300));
+        rulesPanel.setPreferredSize(new Dimension(450,500));
+        outputPanel.setPreferredSize(new Dimension(450,500));
 
         Container container = mainFrame.getContentPane();
         container.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -46,7 +56,7 @@ public class MainWindow extends JFrame {
         menuBar.add(saveFileB);
         mainFrame.setJMenuBar(menuBar);
 
-        mainFrame.setSize(new Dimension(1300,600));
+        mainFrame.setSize(new Dimension(1000,600));
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setTitle("Generator Specyfikacji Logicznej");
@@ -60,7 +70,7 @@ public class MainWindow extends JFrame {
 
         generatedOutput.setText("tu poleci wygenerowana specyfikacja");
         generatedOutput.updateUI();
-        loadedLogicRules.setText("Tu polecą wczytane zasady logiki, wiesz te całe Seq itp");
+        loadedLogicRules.setText("Tu polecą wczytane zasady logiki, m.in Sequence,Concurrent ");
         generatedOutput.updateUI();
     }
     private void loadListeners(){
@@ -72,7 +82,9 @@ public class MainWindow extends JFrame {
                 if(returnVal==JFileChooser.APPROVE_OPTION) {
 
                     String fileName="";
+                    File file=null;
                     try {
+                        file=fileChooser.getSelectedFile();
                         fileName=fileChooser.getSelectedFile().getCanonicalPath();
                     } catch (IOException e1) {
                         e1.printStackTrace();
@@ -83,7 +95,7 @@ public class MainWindow extends JFrame {
 
                         JPanel conPan=new JPanel();
                         conPan.setPreferredSize(new Dimension(400, 300));
-                        parseFileIntoRules(fileName);
+                        parseFileIntoRules(file);
 
                         System.out.println("Loading complete\n Result in output Text Area");
                     }
@@ -99,7 +111,7 @@ public class MainWindow extends JFrame {
 
                 if(returnVal==JFileChooser.APPROVE_OPTION){
                     File fileSelected= fileChooser.getSelectedFile();
-                    File file=new File(String.valueOf(fileSelected));
+                    File file=new File(fileSelected+".txt");
 
                     // creates the file
                     try {
@@ -113,9 +125,8 @@ public class MainWindow extends JFrame {
                     } catch (IOException e1) {
                         e1.printStackTrace();
                     }
-                    //generatedOutput.setText("działaj do k***");
-                    System.out.println(generatedOutput.getText().toString());
-                    while(!generatedOutput.getText().isEmpty()) {
+                   // System.out.println(generatedOutput.getText());
+                    //while(!generatedOutput.getText().isEmpty()) {
                         // Writes the content to the file
                         try {
                             writer.write(generatedOutput.getText().toString());
@@ -134,7 +145,7 @@ public class MainWindow extends JFrame {
                             e1.printStackTrace();
                         }
                         fileChooser.setSelectedFile(new File(""));
-                    }
+                   // }
                 }
             }
 
@@ -147,11 +158,34 @@ public class MainWindow extends JFrame {
         });
     }
 
-    private void parseFileIntoRules(String fileName){
+    private void parseFileIntoRules(File file){
+        loadedLogicRules.setText("Wczytane zasady:\n");
+        System.out.println("Parsuje");
+
+        try {
+            Scanner scanner=new Scanner(file);
+
+            while(scanner.hasNextLine()){
+                loadedFileLines.add(scanner.nextLine());
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        for(String s:loadedFileLines){
+            loadedLogicRules.append("\n"+s);
+        }
+        System.out.println("Wczytano");
 
     }
 
     private void generateSpecLog(){
+        System.out.println("Generuje");
+        generatedOutput.setText("Wygenerowana logika:\n");
+        for(String s:loadedFileLines){
+            generatedOutput.append("\n"+s);
+        }
 
     }
 
